1e3e178ad42663d09f6e75005b578aeb5a6d9907
Timo Makinen <tmakinen@foo.sh>
2026-03-01 20:35:19 +0000

    Add default.env file for global variables like root pw

--- console/build.sh
+++ console/build.sh
@@ -8,7 +8,7 @@ cd "$(dirname "$0")"
 podman run --rm \
     --env "IMG_NAME=console" \
     --env "ADDITIONAL_KERNEL_MODULES=nf_* nft_*" \
-    --env "DEFAULT_ROOT_PASSWORD=secret" \
+    --env-file "$(pwd)/../default.env" \
     --volume "$(pwd)/image.sh:/input/image.sh:z" \
     --volume "$(pwd):/output:z" \
     ghcr.io/raspi-alpine/builder:latest
