bcb3dc2726819af159fa6934355a2970a779173f
Timo Makinen <tmakinen@foo.sh>
2026-03-03 06:56:22 +0000

    Always use latest version of builder

--- console/build.sh
+++ console/build.sh
@@ -5,10 +5,13 @@ umask 022
 
 cd "$(dirname "$0")"
 
+token="$(curl -sSf "https://ghcr.io/token?service=ghcr.io&scope=repository:raspi-alpine/builder:pull" | jq -r ".token")"
+tag="$(curl -sSf -H "Authorization: Bearer ${token}" "https://ghcr.io/v2/raspi-alpine/builder/tags/list" | jq -r '.tags | last')"
+
 podman run --rm \
     --env "IMG_NAME=console" \
     --env "ADDITIONAL_KERNEL_MODULES=nf_* nft_*" \
     --env-file "$(pwd)/../default.env" \
     --volume "$(pwd)/image.sh:/input/image.sh:z" \
     --volume "$(pwd):/output:z" \
-    ghcr.io/raspi-alpine/builder:latest
+    "ghcr.io/raspi-alpine/builder:${tag}"
